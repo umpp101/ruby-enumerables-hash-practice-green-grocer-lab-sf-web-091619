96d2cc89f89f11a44613689c9f8c024564c52c04
Mustafa Muse <umpp101@icloud.com>
2019-09-13 01:01:54 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -22,7 +22,7 @@ def apply_coupons(cart, coupons)
       cart["#{item} W/COUPON"]={:price => coupon[:cost]/coupon[:num],:clearance => cart[item][:clearance],:count => coupon[:num]}
       cart[item][:count] -= coupon[:num]
       
-    elsif cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
+    elsif cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
     
     cart["#{item} W/COUPON"] += coupon[:num]
     cart[item][:count] -= coupon[:num]
