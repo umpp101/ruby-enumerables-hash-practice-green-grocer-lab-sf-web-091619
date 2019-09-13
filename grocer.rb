67194a485fad2e5c0547fc11262042620f5529f4
Mustafa Muse <umpp101@icloud.com>
2019-09-13 02:02:00 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -44,6 +44,6 @@ def checkout(array,coupons)
   hashed_cart = consolidate_cart(array)
   coupons_applied = apply_coupons(hashed_cart, coupons)
   clearance_applied = apply_clearance(coupons_applied)
-total = clearance_applied.reduce(0){ |acc, [key, value]| acc += value[:count] * value[:price]}
+total = clearance_applied.reduce(0){ |acc, [key, value]| acc += value[:count] * value[:price] }
 total > 100 ? total * 0.9 : total
 end
